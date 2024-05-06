d768652ec7828ccd7976f8f8f32b44698c25b4fe
Ahmed Guetat <62658251+ahmedguetat62@users.noreply.github.com>
2024-05-06 22:14:12 +0000

    FINAL mods on ng-otp-input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,11 +13,12 @@ export class AppComponent  implements OnInit {
   otp!: string;
   inputDigitLeft: string = "Verify code";
   btnStatus: string = "btn-light";
-
+  validOtp: any = 0;
   public configOptions = {
     length: 6,
     inputClass: 'digit-otp',
-    containerClass: 'd-flex justify-content-between'
+    containerClass: 'd-flex justify-content-between',
+    allowNumbersOnly:true
   }
   
   ngOnInit() {
@@ -27,6 +29,8 @@ export class AppComponent  implements OnInit {
     this.otp = event;
     if(this.otp.length < this.configOptions.length) {
       this.inputDigitLeft = this.configOptions.length - this.otp.length + " digits Left";
+      this.validOtp = 0;
+      this.updateInputClass()
       this.btnStatus = 'btn-light';
     }
 
@@ -35,4 +39,29 @@ export class AppComponent  implements OnInit {
       this.btnStatus = 'btn-primary';
     }
   }
+  check(){
+    if(this.otp ==="111111"){
+      this.validOtp = 1;
+      this.btnStatus = 'btn-success';
+    }else{
+      this.btnStatus = 'btn-danger';
+      this.validOtp = 2;
+
+    }
+    this.updateInputClass();
+  }
+  updateInputClass() {
+    switch(this.validOtp) {
+      case 0:
+        this.configOptions.inputClass = 'digit-otp';
+        break;
+      case 1:
+        this.configOptions.inputClass = 'digit-otp-success';
+        break;
+      case 2:
+        this.configOptions.inputClass = 'digit-otp-error shake';
+        break;
+    }
+  }
+  
 }
